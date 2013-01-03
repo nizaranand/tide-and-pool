228735f6675159af99703bb9dd2f2a6a323a7f82
Dom Pascal <dom@dompascal.com>
2013-01-03 14:51:04 +0000

    added hello comment to wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,8 @@
  * @package WordPress
  */
 
+/* hello! */ 
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'tideandpool');
