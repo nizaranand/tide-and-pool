1914a165b733cfec70bcdb57da862b577756859f
Dom Pascal <dom@dompascal.com>
2013-01-20 23:21:31 +0000

    Re-added quantity input with add/minus buttons on product view page.
    Signed-off-by: Dom Pascal <dom@dompascal.com>

--- wp-content/themes/tideandpool/woocommerce/single-product/add-to-cart/quantity.php
+++ wp-content/themes/tideandpool/woocommerce/single-product/add-to-cart/quantity.php
@@ -7,16 +7,16 @@
  * @version     1.6.4
  */
 ?>
+<!--
 <div class="select-wrapper">
 	<select>
-		<optgroup>
-		<option value="1">Qty: 1</option>
-		<option value="2">Qty: 2</option>
-		<option value="3">Qty: 3</option>
-		<option value="4">Qty: 4</option>
-		<option value="5">Qty: 5</option>
-		<option value="6">Qty: 6</option>
-		</optgroup>
+		<option class="input-text qty text" value="1" name="quantity">Qty: 1</option>
+		<option class="input-text qty text" value="2" name="quantity">Qty: 2</option>
+		<option class="input-text qty text" value="3" name="quantity">Qty: 3</option>
+		<option class="input-text qty text" value="4" name="quantity">Qty: 4</option>
+		<option class="input-text qty text" value="5" name="quantity">Qty: 5</option>
+		<option class="input-text qty text" value="6" name="quantity">Qty: 6</option>
 	</select>
-<!-- <input name="<?php echo $input_name; ?>" data-min="<?php echo $min_value; ?>" data-max="<?php echo $max_value; ?>" value="<?php echo $input_value; ?>" size="4" title="Qty" class="input-text qty text" maxlength="12" /> -->
 </div>
+-->
+<div class="quantity"><input name="<?php echo $input_name; ?>" data-min="<?php echo $min_value; ?>" data-max="<?php echo $max_value; ?>" value="<?php echo $input_value; ?>" size="4" title="Qty" class="input-text qty text" maxlength="12" /></div>
