d8d78b0a62875fa1755cc28b700d028342ef76e8
Dom Pascal <dom@dompascal.com>
2013-01-21 00:03:53 +0000

    Re-uploaded product images to the correct size for magic zoom.
    Signed-off-by: Dom Pascal <dom@dompascal.com>

--- wp-content/themes/tideandpool/woocommerce/cart/empty.php
+++ wp-content/themes/tideandpool/woocommerce/cart/empty.php
@@ -8,8 +8,7 @@
  */
 ?>
 
-<p><?php _e('Your cart is currently empty.', 'woocommerce') ?></p>
-
-<?php do_action('woocommerce_cart_is_empty'); ?>
-
-<p><a class="button" href="<?php echo get_permalink(woocommerce_get_page_id('shop')); ?>"><?php _e('&larr; Return To Shop', 'woocommerce') ?></a></p>
+<div class="site-messages">
+	<p><?php _e('Your cart is currently empty.', 'woocommerce') ?>
+	<?php do_action('woocommerce_cart_is_empty'); ?> <a class="button" href="<?php echo get_permalink(woocommerce_get_page_id('shop')); ?>"><?php _e('&larr; Return To Shop', 'woocommerce') ?></a></p>
+</div>
