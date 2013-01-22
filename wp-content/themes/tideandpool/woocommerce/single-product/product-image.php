2f86072dbc460d887cb7ba507668e5745e06fb47
Dom Pascal <dom@dompascal.com>
2013-01-22 17:36:36 +0000

    Synced magic zoom image gallery with fancy box 'view larger' gallery.
    Signed-off-by: Dom Pascal <dom@dompascal.com>

--- wp-content/themes/tideandpool/woocommerce/single-product/product-image.php
+++ wp-content/themes/tideandpool/woocommerce/single-product/product-image.php
@@ -18,10 +18,11 @@ global $post, $woocommerce;
 		<?php $images = get_field('product_gallery'); if( $images ): ?>
 		<?php foreach( $images as $image ): ?>
 		<article>
-			<a href="<?php echo $image['url']; ?>" rel="zoom-id:zoom;" class="selector" rev="<?php echo $image['sizes']['large']; ?>">
+			<a id="<?php echo $image['id']; ?>" href="<?php echo $image['url']; ?>" rel="zoom-id:zoom;" class="selector" rev="<?php echo $image['sizes']['large']; ?>">
 				<img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
 			</a>
 		</article>
+		
 		<?php endforeach; ?>	
 		<?php endif; ?>
 	</section>
@@ -42,14 +43,25 @@ global $post, $woocommerce;
 <div class="clear"></div>
 
 <!-- view larger btn -->
-<a class="view-larger fancybox" rel="gallery1" href="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>">
+
+<?php if ( has_post_thumbnail() ) : ?>
+<a class="view-larger fancybox" href="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>">
 	<img src="<?php bloginfo( 'template_url' ); ?>/css/img/view-larger-btn.png">
-</a>		
-<?php $images = get_field('product_gallery'); if( $images ): ?>
-<?php foreach( $images as $image ): ?>
-<a class="view-larger fancybox" rel="gallery1" href="<?php echo $image['sizes']['large']; ?>" title=""></a>
-<?php endforeach; ?>	
+</a>
 <?php endif; ?>
+
+<script type="text/javascript">
+	$(function() {
+		<?php $images = get_field('product_gallery'); if( $images ): ?>
+		<?php foreach( $images as $image ): ?>
+		$('a#<?php echo $image['id']; ?>').click(function() {
+			$("a.view-larger").attr("href", "<?php echo $image['url']; ?>");
+		});
+		<?php endforeach; ?>
+		<?php endif; ?>
+	});	
+</script>
+
 <!-- end view larger -->
 
 <!-- <?php do_action('woocommerce_product_thumbnails'); ?> -->
