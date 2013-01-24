f1c96ed6ffeec21b88a330f1eb2ca45c299926e6
Dom Pascal <dom@dompascal.com>
2013-01-24 23:07:56 +0000

    Added google meta tracking tag to header.php
    Signed-off-by: Dom Pascal <dom@dompascal.com>

--- wp-content/themes/tideandpool/header.php
+++ wp-content/themes/tideandpool/header.php
@@ -17,6 +17,7 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="google-site-verification" content="rSrZDH1AkYFFyT9-b5etOn1VfCtXxJak5FwFW_jwDc8" />
 <meta name="viewport" content="initial-scale=1, width=device-width, user-scalable=no, maximum-scale=1.0">
 <meta name="author" content="info@blackandblackcreative.com">
 <meta name="distribution" content="Global">
