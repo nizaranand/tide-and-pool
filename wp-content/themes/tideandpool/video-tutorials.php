84cd39569f8361404ee83ad6d9c05b7d6362fb5b
Dom Pascal <dom@dompascal.com>
2013-01-31 07:59:19 +0000

    Created video tutorials template & updated mySQL database.
    Signed-off-by: Dom Pascal <dom@dompascal.com>

---
+++ wp-content/themes/tideandpool/video-tutorials.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Template Name: Video Tutorials
+ *
+ */
+
+get_header(); ?>
+<script src="http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/swfobject.js" type="text/javascript"></script>
+
+<!-- video tutorials -->
+<section id="video-tutorials">
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Updating/Adding Products</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-1" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
+			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
+			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-1");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Updating Pages (About & Customer Service Sections)</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-2" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
+			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
+			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-2");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Updating/Adding Boutiques</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-3" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
+			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
+			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-3");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Updating the collections page</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-4" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
+			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
+			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-4");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+</section>
+<!-- end video tutorials -->
+
+<?php get_footer(); ?>
