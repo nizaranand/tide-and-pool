054b0f5b72eed0e77859d7aca535aa5666658635
Dom Pascal <dom@dompascal.com>
2013-01-31 22:34:02 +0000

    Finished video tutorials template.
    Signed-off-by: Dom Pascal <dom@dompascal.com>

--- wp-content/themes/tideandpool/video-tutorials.php
+++ wp-content/themes/tideandpool/video-tutorials.php
@@ -5,22 +5,22 @@
  */
 
 get_header(); ?>
-<script src="http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/swfobject.js" type="text/javascript"></script>
+<script src="<?php bloginfo( 'template_url' ); ?>/js/vendor/swfobject.js" type="text/javascript"></script>
 
 <!-- video tutorials -->
 <section id="video-tutorials">
 	<!-- video panel -->
 	<article class="video-panel">
 		<!-- header -->
-		<h2>Updating/Adding Products</h2>
+		<h2>Update Home Page</h2>
 		<!-- end header -->
 			
 		<!-- video -->
 		<div id="video-1" class="video"></div>
 		<script type="text/javascript">
-			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
-			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
-			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/update-home-page_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
 			//so.addVariable("autoplay","true");
 			so.addParam("allowFullScreen","true");
 			so.write("video-1");
@@ -32,15 +32,15 @@ get_header(); ?>
 	<!-- video panel -->
 	<article class="video-panel">
 		<!-- header -->
-		<h2>Updating Pages (About & Customer Service Sections)</h2>
+		<h2>Update Product Entry for 'Our Towels' Carousel/Slideshow</h2>
 		<!-- end header -->
 			
 		<!-- video -->
 		<div id="video-2" class="video"></div>
 		<script type="text/javascript">
-			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
-			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
-			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/add-new-product-to-our-towels_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
 			//so.addVariable("autoplay","true");
 			so.addParam("allowFullScreen","true");
 			so.write("video-2");
@@ -52,15 +52,15 @@ get_header(); ?>
 	<!-- video panel -->
 	<article class="video-panel">
 		<!-- header -->
-		<h2>Updating/Adding Boutiques</h2>
+		<h2>Add New Product</h2>
 		<!-- end header -->
 			
 		<!-- video -->
 		<div id="video-3" class="video"></div>
 		<script type="text/javascript">
-			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
-			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
-			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/add-new-product_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
 			//so.addVariable("autoplay","true");
 			so.addParam("allowFullScreen","true");
 			so.write("video-3");
@@ -72,18 +72,98 @@ get_header(); ?>
 	<!-- video panel -->
 	<article class="video-panel">
 		<!-- header -->
-		<h2>Updating the collections page</h2>
+		<h2>Update Product Entry for Product View Page</h2>
 		<!-- end header -->
 			
 		<!-- video -->
 		<div id="video-4" class="video"></div>
 		<script type="text/javascript">
-			var so = new SWFObject("http://taniaedwardscomedy.com/wp-content/themes/tania-edwards-comedy/js/mpw_player.swf", "swfplayer", "700", "500", "9", "#000000");
-			so.addVariable("flv", "http://taniaedwardscomedy.com/videos/flv/update-press_F8_FLV_VP6_3000Kbps_720p.flv");
-			//so.addVariable("jpg","http://taniaedwardscomedy.com/videos/previews/imagen.jpg");
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/update-product-view_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
 			//so.addVariable("autoplay","true");
 			so.addParam("allowFullScreen","true");
 			so.write("video-4");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Update Collections Page</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-5" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/collections-page_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-5");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Update About Page</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-6" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/about-page_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-6");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Update Boutiques Page</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-7" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/editing-boutiques_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-7");
+		</script>
+		<!-- end video -->
+	</article>
+	<!-- end video panel -->
+	
+	<!-- video panel -->
+	<article class="video-panel">
+		<!-- header -->
+		<h2>Update Customer Service Popup</h2>
+		<!-- end header -->
+			
+		<!-- video -->
+		<div id="video-8" class="video"></div>
+		<script type="text/javascript">
+			var so = new SWFObject("<?php bloginfo( 'template_url' ); ?>/js/vendor/mpw_player.swf", "swfplayer", "1280", "800", "9", "#000000");
+			so.addVariable("flv", "http://tide.blackandblackcloud.com/flv/update-customer-service-popup_F8_1Mbps.flv");
+			so.addVariable("jpg","<?php bloginfo( 'template_url' ); ?>/css/img/video-preview.jpg");
+			//so.addVariable("autoplay","true");
+			so.addParam("allowFullScreen","true");
+			so.write("video-8");
 		</script>
 		<!-- end video -->
 	</article>
